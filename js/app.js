013f9cbaf08199758f41a38f58fa3af8c91c0c1a
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-08-28 22:51:00 +0300

    toLocaleString

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 const comments = JSON.parse(DATA);
 const showcaseEl = document.getElementById("showcase");
+const searchFormEl = document.getElementById('searchForm')
 
 
-
+searchFormEl.addEventListener('submit', event => {
+  event.preventDefault()
+  // console.log(event.target);
+  // console.log(event.target.search);
+  // console.log(event.target.search.value);
+  const query = event.target.search.value.trim().toLowerCase().split(' ').filter(word => !!word)
+  console.log(query);
+  const searchFields = ["name", "phone", "text"]
+  const filteredСomments = comments.filter(comments => {
+    return query.every(word => {
+      return searchFields.some(field => {
+        return String(comments[field]).toLowerCase().includes(word)
+      })
+    })
+  })
+  console.table(filteredСomments);
+})
 
 renderCardShowcase(comments, showcaseEl);
 
@@ -29,13 +46,15 @@ function renderCardShowcase(cardsDataArr, cardShowcaseEl) {
   function createCardHTML(cardDataObj) {
       
   return `<div class="comments">
-    <img class="comments-img" src="${cardDataObj.avatar}" width="1" height="1" loading="lazy" decoding="async">
+    <img class="comments-img" src="${cardDataObj.avatar}" alt="${cardDataObj.name}" width="1" height="1" loading="lazy" decoding="async">
   <div class="comments-content">
     <h2 class="comments-author">${cardDataObj.name}</h2>
-    <a class="comments-link" href="#">${cardDataObj.phone}</a>
+    <a class="comments-link" href="#">Тел: ${cardDataObj.phone}</a>
     <p class="comments-text">${cardDataObj.text}
     </p>
 <p class="comments-date">Добавлено: ${new Date(cardDataObj.date).toLocaleString()}</p>
   </div>
   </div> `;
 }
+
+
