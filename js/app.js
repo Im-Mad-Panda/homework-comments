22a0944a00c897e4a67383ffe1efcee6f137dd7b
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-10-15 01:57:12 +0300

    getComments

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
+let comments = [];
+console.log(comments);
+const showcaseEl = document.getElementById("showcase");
+const searchFormEl = document.getElementById('searchForm')
+const smsCounterAllEl = document.getElementById('smsCounterAll')
+const smsCounterUnseenEl = document.getElementById('smsCounterUnseen')
 
 
-getData('//localhost:1234/todos/10')
-// getData('https://www.ukr.net/dat/smart/struct.9.json?_=1633868174127')
 
 
 
+getComments()
 
-async function getData(url) {
+async function getComments() {
   try {
-    const response = await fetch(url);
-    console.log(response);
+    const response = await fetch('/data/comments.json');
     if (response.ok) {
         const json = await response.json();
-        console.log(json);
+        comments = json
+        renderCardShowcase(comments, showcaseEl);
     } else{
         throw `Bad response! ${response.statusText} (${response.status})`
     }
@@ -21,29 +26,64 @@ async function getData(url) {
   }
 }
 
-
-postData('//localhost:1234/todos', {
-    title: Math.random(),
-    status: false
-})
-
-async function postData(url, data) {
-    try {
-      const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-              "Content-type": 'application/json;charset=utf-8'
-          },
-          body: JSON.stringify(data)
-      });
-      console.log(response);
-      if (response.ok) {
-          const json = await response.json();
-          console.log(json);
-      } else{
-          throw `Bad response! ${response.statusText} (${response.status})`
+showcaseEl.addEventListener('click', e => {
+  const messageEl = e.target.closest('.comments')
+  if (messageEl) {
+    const messageId = messageEl.dataset.id
+    const messageIdx = comments.findIndex(comment => comment.id == messageId)
+    if (messageIdx !== -1) {
+      const message = comments[messageIdx]
+      if (message.seen) {
+        comments.splice(messageIdx, 1)
+      } else {
+        message.seen = true
       }
-    } catch (error) {
-      console.warn(error);
     }
   }
+  renderCardShowcase(comments, showcaseEl);
+})
+
+
+searchFormEl.addEventListener('submit', event => {
+  event.preventDefault()
+  const query = event.target.search.value.trim().toLowerCase().split(' ').filter(word => !!word)
+  console.log(query);
+  const searchFields = ["name", "phone", "text"]
+  const filteredСomments = comments.filter(comment => {
+    return query.every(word => {
+      return searchFields.some(field => {
+        return String(comment[field]).toLowerCase().includes(word)
+      })
+    })
+  })
+  console.table(filteredСomments);
+  renderCardShowcase(filteredСomments, showcaseEl);
+})
+
+
+
+function renderCardShowcase(cardsDataArr, cardShowcaseEl) {
+  smsCounterAllEl.textContent = cardsDataArr.length
+  smsCounterUnseenEl.textContent = cardsDataArr.filter(cardDataObj => !cardDataObj.seen).length
+  cardsDataArr.sort((a, b) => a.seen - b.seen || b.date - a.date)
+  cardShowcaseEl.innerHTML = createCardShowcaseHTML(cardsDataArr).join("");
+}
+
+function createCardShowcaseHTML(cardsDataArr) {
+  return cardsDataArr.map((cardDataObj) => createCardHTML(cardDataObj));
+}
+
+
+function createCardHTML(cardDataObj) {
+
+  return `<div class="comments ${cardDataObj.seen ? 'seen' : ''}" data-id="${cardDataObj.id}">
+    <img class="comments-img" src="${cardDataObj.avatar}" alt="${cardDataObj.name}" width="1" height="1" loading="lazy" decoding="async">
+    <div class="comments-content">
+    <h2 class="comments-author">${cardDataObj.name}</h2>
+    <a class="comments-link" href="tel:${cardDataObj.phone}">Тел: ${cardDataObj.phone}</a>
+    <p class="comments-text">${cardDataObj.text}
+    </p>
+  <p class="comments-date">Добавлено: ${new Date(cardDataObj.date).toLocaleString()}</p>
+  </div>
+  </div> `;
+}
